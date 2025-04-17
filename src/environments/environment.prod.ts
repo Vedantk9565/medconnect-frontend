f5a5a5762322ab0cf0a7596f6c9dbcbbbec7a794
Vedantk9565 <vedantkadam1900@gmail.com>
2025-04-17 18:46:18 +0530

    Fixed prod environment and removed unused import

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,8 @@
-import { BASE_URL } from "../app/constants";
 
 export const environment = {
     production: true,
     apiUrl: 'https://medconnect-backend-283p.onrender.com',
-     BASE_URL :'https://medconnect-backend-283p.onrender.com'
-
+     
 
 };
 
