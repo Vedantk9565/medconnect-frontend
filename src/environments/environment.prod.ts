600fdeb773ce0497beb187510b54dafa4adf76ae
Vedantk9565 <vedantkadam1900@gmail.com>
2025-04-17 15:59:46 +0530

    updated baseurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
+import { BASE_URL } from "../app/constants";
+
 export const environment = {
     production: true,
-    apiUrl: 'https://medconnect-backend-283p.onrender.com'
-    
+    apiUrl: 'https://medconnect-backend-283p.onrender.com',
+     BASE_URL :'https://medconnect-backend-283p.onrender.com'
+
 
 };
 
