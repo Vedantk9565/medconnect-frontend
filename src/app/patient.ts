92be2f8371131c99e4723f53a15bcdff876267fe
Vedantk9565 <vedantkadam1900@gmail.com>
2025-04-20 12:40:50 +0530

    fixed drugName

--- src/app/patient.ts
+++ src/app/patient.ts
@@ -1,10 +1,11 @@
+import { Prescription } from "./prescription";
 
 export class Patient {
     id:number=0;
     name:string="";
     age:string="";
     blood:string="";
-    prescription: { drugName: string, timeToTake: string }[] = [];
+    prescription: Prescription[] = [];
     dose:string="";
     fees:string="";
     urgency:string="";
