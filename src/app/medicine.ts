7e41dd93e6bd0b37be8a86d3ad1483b5754cce35
Vedantk9565 <vedantkadam1900@gmail.com>
2025-04-20 12:31:40 +0530

    fixed drugName

--- src/app/medicine.ts
+++ src/app/medicine.ts
@@ -1,7 +1,9 @@
-export class Medicine {
-    id: number = 0;
-    drugName: string = "";
-    stock: string = "";
-    isSelected?: boolean;
-    timeToTake?: string;
+export class Medicine 
+{
+    id:number=0;
+    drugName:string="";
+    stock:String="";
+    isSelected?: boolean; 
+    timeToTake?: string[] = [];
+    
 }
--- src/app/patient.ts
+++ src/app/patient.ts
@@ -1,13 +1,13 @@
-import { Prescription } from "./prescription"; // adjust path as needed
 
 export class Patient {
-  id: number = 0;
-  name: string = "";
-  age: string = "";
-  blood: string = "";
-  prescription: Prescription[] = []; // ✅ now strongly typed
-  dose: string = "";
-  fees: string = "";
-  urgency: string = "";
-  phoneNumber: string = "";
+    id:number=0;
+    name:string="";
+    age:string="";
+    blood:string="";
+    prescription: { drugName: string, timeToTake: string }[] = [];
+    dose:string="";
+    fees:string="";
+    urgency:string="";
+    phoneNumber: string="";
+
 }
